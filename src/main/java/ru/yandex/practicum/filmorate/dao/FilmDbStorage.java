ad99d948f3347d8d4c643169ed7192a3ec164fa9
Elena Shilova <elena.shilova.al@gmail.com>
2025-01-08 00:15:21 +0300

    выполнение задания 12 спринта

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -90,7 +90,7 @@ public class FilmDbStorage implements FilmStorage {
                         try {
                             List<Long> idGenre = jdbcTemplate.queryForList("SELECT genre_id FROM genre WHERE film_id = ?", Long.class, film.getId());
 
-                            if(idGenre.size() == 0 || !idGenre.contains(g.getId())) {
+                            if (idGenre.size() == 0 || !idGenre.contains(g.getId())) {
                                 jdbcTemplate.update(
                                         "INSERT INTO genre (film_id, genre_id) VALUES (?, ?)",
                                         film.getId(), g.getId());
