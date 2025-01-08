8f1c815f6a36742246850a580efac7ddcb406c55
Elena Shilova <elena.shilova.al@gmail.com>
2025-01-08 23:39:44 +0300

    выполнение задания 12 спринта коррект

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.ElementNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -11,9 +13,12 @@ import ru.yandex.practicum.filmorate.mapper.UserRowMapper;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 
 @Component("userDbStorage")
 @Primary
@@ -55,17 +60,19 @@ public class UserDbStorage implements UserStorage {
     @Override
     public User create(User user) {
 
-        jdbcTemplate.update(
-                "INSERT INTO users (email, login, name, birthday) VALUES (?, ?, ?, ?)",
-                user.getEmail(), user.getLogin(),
-                user.getName(), user.getBirthday());
+        String sqlQuery = "insert into users (email, login, name, birthday) values (?, ?, ?, ?)";
 
-        user.setId(
-                jdbcTemplate.queryForObject(
-                        "SELECT MAX(id) FROM users",
-                        Long.class
-                )
-        );
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement stmt = connection.prepareStatement(sqlQuery, new String[]{"ID"});
+            stmt.setString(1, user.getEmail());
+            stmt.setString(2, user.getLogin());
+            stmt.setString(3, user.getName());
+            stmt.setDate(4, Date.valueOf(user.getBirthday()));
+            return stmt;
+        }, keyHolder);
+        user.setId(Objects.requireNonNull(keyHolder.getKey()).longValue());
+
         return user;
     }
 
